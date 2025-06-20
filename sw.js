1f8b15a02890133a71d3f33a532592b94083e45c
ugandazaabu <ugandawebdev@gmail.com>
2025-06-20 21:44:27 +0300

    nnn

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'farm-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
